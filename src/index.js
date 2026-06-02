8d6e10fe34fd6b2c7222cdc63c487156d29a7ef9
agustingraf <aguss.graf@gmail.com>
2026-06-02 10:55:00 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './src/App.js';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+console.log("hello");
